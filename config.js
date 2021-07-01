c044e4f56682ac4d6ca326fe851eacebafaa557c
may-0405 <86756783+may-0405@users.noreply.github.com>
2021-07-01 10:21:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,25 @@
 const CONFIG = {
-    introTitle: 'Anhiu à!',
-    introDesc: `Chỉ cần ngồi với anh 
-chút thôi, được không ?
-Nhìn vào đôi mắt anh
-Thấy sao ? Một thế giới màu hồng
-Dù môi đang cách nhau, cách nhau  , một chút
-Nhưng mà vì em chưa muốn phiêu, muốn phiêu
-Dù thích anh hơi nhiều`,
+    introTitle: 'Hello Phường hihi!',
+    introDesc: `Xe đạp lách cách tôi vẫn chưa quen
+Đường thì tối chơi vơi còn tôi vẫn đứng đợi
+Em nhẹ bước đến mi đã thôi hoen
+Trời trở gió heo may vì tôi đã lỡ yêu em
+Cũng may đường về nhà em quá xa
+Tôi mới được trông ngóng em buông lời hát
+Nhớ thương ngại ngùng nhìn em thoáng qua
+Hạ ơi đứng trôi mãi, mặc kệ em với tôi đi về
+Mới chỉ nhìn em khóc
+Tôi bỗng chợt nhận ra đã yêu em rồi`,
     btnIntro: 'hihi',
-    title: 'Anh có yêu em không nhờ? 😙',
-    desc: 'Hay muốn ăn đấm hả? ',
-    btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, em gái à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em thế nhờ? <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'Chừng nào em hứng em gửi voice em hát cho.',
+    title: 'Thấy Bình ghê hôn? 😙',
+    desc: 'Haha ',
+    btnYes: 'Buộc ghê rồi Phường này <33',
+    btnNo: 'Không ghê cũng ghê :3',
+    question:'Trên thế giới hơn 7 tỉ người mà tụi mình gặp nhau <3',
+    btnReply: 'Gửi cho Mây <3',
+    reply: 'Tại có duyên á <33333333',
+    mess: 'Ông trời sắp đặt buộc chịu nha 😘😘',
+    messDesc: 'Quá xịn <3 .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
